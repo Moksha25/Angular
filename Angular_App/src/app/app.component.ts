240f1c0b2539c718d5039ad2b2e4e6a2661f280b
Mokshini Baglekar <baglekarmokshini03@gmail.com>
2025-03-06 16:22:05 +0530

    [Mokshini Baglekar] Add: Ability to launch bridgelabz site in a new tab on clicking bridgelabz logo.

--- Angular_App/src/app/app.component.ts
+++ Angular_App/src/app/app.component.ts
@@ -5,11 +5,18 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'HelloWorld';
-  imgUrl = "logo.jpg"
+  title = "HelloWorld";
+  imgUrl = "logo.jpg";
+  url = "https://www.bridgelabz.com";
 
   ngOnInit(): void {
-    this.title = "Hello from BridgeLabz.";
+    this.title = "Hello from BridgeLabz";
+  }
+
+  onClick($event: Event) {
+    console.log("Save button is clicked!", $event);
+    window.open(this.url, "_blank");
   }
 }
